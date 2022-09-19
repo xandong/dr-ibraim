aaa4c74725319e1a7206b0cf4763bfae0719c498
Alexandre Gurgel <94716639+xandong@users.noreply.github.com>
2022-09-19 13:58:09 -0300

    add environment variable

--- src/components/MailchimpFormContainer..tsx
+++ src/components/MailchimpFormContainer..tsx
@@ -1,16 +1,12 @@
 import MailchimpSubscribe, { EmailFormFields } from "react-mailchimp-subscribe";
 import { CustomForm } from "./CustomForm";
-import dotenv from "dotenv";
-dotenv.config();
 
 export function MailchimpFormContainer() {
-  const U = import.meta.env.MAILCHIMP_U
-    ? import.meta.env.MAILCHIMP_U
-    : process.env.MAILCHIMP_U;
+  const U = import.meta.env.MAILCHIMP_U;
+
   // const U = "37d01d60608062b84374bc1b7";
-  const ID = import.meta.env.MAILCHIMP_ID
-    ? import.meta.env.MAILCHIMP_ID
-    : process.env.MAILCHIMP_ID;
+  const ID = import.meta.env.MAILCHIMP_ID;
+
   // const ID = "84b02c1391";
 
   const URL = `https://gmail.us17.list-manage.com/subscribe/post?u=${U}&id=${ID}`;
